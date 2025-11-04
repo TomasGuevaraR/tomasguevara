aac4c5fbf60de662492df6e882c54b628b313f87
TomasGuevaraR <tomasguevararamos@gmail.com>
2025-11-04 18:05:55 -0500

    Initial commit

---
+++ src/React/SkillsList.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+
+const CategoryIcons = {
+  "Analysis": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+      <path d="M2 3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H2.992A.993.993 0 0 1 2 20.007V3.993zM4 5v14h16V5H4zm2 2h6v2H6V7zm0 4h8v2H6v-2zm0 4h4v2H6v-2zm10-5.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"></path>
+    </svg>
+  ),
+  "Software Development": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+      <path d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66L14.212 3h2.128L9.788 21z"></path>
+    </svg>
+  ),
+  "UI/UX Design & Prototyping": (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-[var(--sec)] opacity-70"
+    >
+      <path d="M5.7646 7.99998L5.46944 7.26944C5.26255 6.75737 5.50995 6.17454 6.02202 5.96765L15.2939 2.22158C15.8059 2.01469 16.3888 2.26209 16.5956 2.77416L22.2147 16.6819C22.4216 17.194 22.1742 17.7768 21.6622 17.9837L12.3903 21.7298C11.8783 21.9367 11.2954 21.6893 11.0885 21.1772L11.0002 20.9586V21H7.00021C6.44792 21 6.00021 20.5523 6.00021 20V19.7303L2.65056 18.377C2.13849 18.1701 1.89109 17.5873 2.09798 17.0752L5.7646 7.99998ZM8.00021 19H10.2089L8.00021 13.5333V19ZM6.00021 12.7558L4.32696 16.8972L6.00021 17.6084V12.7558ZM7.69842 7.44741L12.5683 19.5008L19.9858 16.5039L15.1159 4.45055L7.69842 7.44741ZM10.6766 9.47974C10.1645 9.68663 9.5817 9.43924 9.37481 8.92717C9.16792 8.4151 9.41532 7.83227 9.92739 7.62538C10.4395 7.41849 11.0223 7.66588 11.2292 8.17795C11.4361 8.69002 11.1887 9.27286 10.6766 9.47974Z"></path>
+    </svg>
+  ),
+};
+
+const SkillsList = () => {
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
+  const skills = {
+    "Analysis": [
+      "Software requirements analysis",
+      "Technical and functional documentation",
+      "Business process modeling",
+      "Database analysis and design",
+    ],
+    "Software Development": [
+      "Full-stack web applications (Frontend & Backend)",
+     /* "Cross-platform mobile applications",*/
+      "Enterprise management systems",
+    ],
+    "UI/UX Design & Prototyping": [
+      "UI design with Figma & Canva",
+      "UX research & improvements",
+      "Prototyping for websites & mobile apps",
+    ],
+  };
+
+  const toggleItem = (item: string) => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
+  return (
+    <div className="text-left pt-3 md:pt-9">
+      <h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
+        What I do?
+      </h3>
+      <ul className="space-y-4 mt-4 text-lg">
+        {Object.entries(skills).map(([category, items]) => (
+          <li key={category} className="w-full">
+            <div
+              onClick={() => toggleItem(category)}
+              className="md:w-[400px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
+            >
+              <div className="flex items-center gap-3 p-4">
+                {CategoryIcons[category]}
+                <div className="flex items-center gap-2 flex-grow justify-between">
+                  <div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
+                    <span className="block truncate text-[var(--white)] text-lg">
+                      {category}
+                    </span>
+                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className={`w-6 h-6 text-[var(--white)] transform transition-transform flex-shrink-0 ${
+                      openItem === category ? "rotate-180" : ""
+                    }`}
+                  >
+                    <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div
+                className={`transition-all duration-300 px-4 ${
+                  openItem === category
+                    ? "max-h-[500px] pb-4 opacity-100"
+                    : "max-h-0 opacity-0"
+                }`}
+              >
+                <ul className="space-y-2 text-[var(--white-icon)] text-sm">
+                  {items.map((item, index) => (
+                    <div key={index} className="flex items-center">
+                      <span className="pl-1">•</span>
+                      <li className="pl-3">{item}</li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SkillsList;
